1e4632eecb4a19877746c6b23ebe6a105b5dbeee
aurèle <aurelien.plazzotta@tutanota.com>
2023-10-20 21:05:19 +0200

    Check skillPointsRemaining to prevent it to fall under 0.

--- character_creation.js
+++ character_creation.js
@@ -3,11 +3,12 @@
 // HTML tags' selections.
 const btnTrain = document.querySelector('.train');
 const skillsElm = document.querySelectorAll('.skill');
-const skillElm = document.querySelector('.skill');
 const skills = ['strength', 'dexterity', 'stamina', 'luck'];
 // const skillPointsRemaining = document.querySelector('.skill-points-remaining')
+
+// State variables.
 let skillPointsRemaining = 20;
-let skillPointsSpent = false;
+let allSkillPointsSpent = false;
 
 const addSkillPoints = function (/** @type {string} */ skill) {
   /**
@@ -16,53 +17,30 @@ const addSkillPoints = function (/** @type {string} */ skill) {
   return Number(document.getElementById(`${skill}`).value);
 };
 
+/* TODO: prevent skill points remaining to fall under 0 
+by checking all `skillElm.value` when clicking `train` button.
+*/
 // for (let i = 0; i < skillsElm.length; ++i) {
 skillsElm.forEach((skillElm, index) => {
   btnTrain.addEventListener('click', function () {
-    if (!skillPointsSpent) {
-      const skillPoints = addSkillPoints(`${skills[index]}`); // TODO adapt for each input area!
+    skillPointsRemaining -= Number(skillElm.value);
+
+    if (!allSkillPointsSpent) {
+      const skillPoints = addSkillPoints(`${skills[index]}`);
 
       if (skillPoints > 0 && skillPoints < 6) {
         document.querySelector(`.skill-score-${index}`).textContent = skillPoints;
-        skillPointsRemaining -= skillPoints;
-        if (skillPointsRemaining) {
+
+        if (skillPointsRemaining > 0) {
+          skillPointsRemaining -= skillPoints;
           document.querySelector('.skill-points-remaining').textContent = skillPointsRemaining;
-        } else if (!skillPointsRemaining) {
-          document.querySelector('.points-left').textContent = 'No skill points available.';
-          skillPointsSpent = true;
+        } else {
+          document.querySelector('.points-left').textContent = 'ℹ️ No skill points available.';
+          allSkillPointsSpent = true;
         }
+
+        // TODO add a <p> to warn user to enter a score between 1 and 5 for each wrong skill value.
       }
-      // TODO add a <p> to warn user to enter a score between 1 and 5.
     }
   });
 });
-
-// btnTrain.addEventListener('click', function () {
-//   if (!skillPointsSpent) {
-//     const skillPoints = addSkillPoints();
-
-//     if (skillPoints > 0 && skillPoints < 6) {
-//       document.querySelector('.skill-score-0').textContent = skillPoints;
-//       skillPointsRemaining -= skillPoints;
-//       if (skillPointsRemaining) {
-//         document.querySelector('.skill-points-remaining').textContent = skillPointsRemaining;
-//       } else if (!skillPointsRemaining) {
-//         document.querySelector('.points-left').textContent = 'No skill points available.';
-//         skillPointsSpent = true;
-//       }
-//     }
-//     // TODO add a <p> to warn user to enter a score between 1 and 5.
-//   }
-// });
-
-// btnTrain.addEventListener('click', function () {
-//   // skillsElm.forEach(skillElm => {
-//   for (let i = 0; i < skillsElm.length; ++i) {
-//     if (skillsElm[i] > 1 || skillsElm[i] < 6) {
-//       document.querySelector(`.skill-score-${i}`).textContent = Number(
-//         document.querySelector(`skills[${i}]`).value
-//       );
-//       btnTrain.classList.add('hidden');
-//     }
-//   }
-// });
