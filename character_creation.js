f47f073b5201ac937ce251d2b699eeef3d1e8504
aurèle <aurelien.plazzotta@tutanota.com>
2023-10-22 21:57:58 +0200

    New attempt to fix checking user input values

--- character_creation.js
+++ character_creation.js
@@ -3,17 +3,16 @@
 // HTML tags' selections.
 const btnTrain = document.getElementById('train');
 const skillsElm = document.querySelectorAll('.skill');
-const skills = ['strength', 'dexterity', 'stamina', 'luck'];
+const skills = ['strength', 'dexterity', 'stamina', 'luck', 'wisdom'];
 const skillInfoElm = document.getElementById('skill-points-info');
 
 // State variables.
 let skillPointsRemaining = 20;
 let allSkillPointsSpent = false;
-let debug = false;
 
 const addSkillPoints = function (/** @type {string} */ skill) {
   /**
-   * Add skill points to a total skill's score.
+   * Add skill points to a total skill's level.
    */
   return Number(document.getElementById(`${skill}`).value);
 };
@@ -21,35 +20,36 @@ const addSkillPoints = function (/** @type {string} */ skill) {
 /* TODO: prevent skill points remaining to fall under 0 
 by checking all `skillElm.value` when clicking `train` button.
 */
-btnTrain.addEventListener('click', function () {
-  skillsElm.forEach((skillElm, index) => {
-    // FIXME calcul is wrong
-    // skillPointsRemaining -= Number(skillElm.value);
-    // if (debug) console.log(`skillPointsRemaining: ${skillPointsRemaining}`);
+skillsElm.forEach((skillElm, index) => {
+  btnTrain.addEventListener('click', function () {
     if (!allSkillPointsSpent) {
       const skillPoints = addSkillPoints(`${skills[index]}`);
-      if (debug) console.info(`skillPoints: ${skillPoints}`);
 
       // Check either enough skill points remains to assign a new skill value.
-      if (skillPointsRemaining - skillPoints > 0) {
-        // Check either a new skill value fits in the correct range.
-        if (skillPoints >= 1 && skillPoints <= 5) {
-          document.querySelector(`.skill-score-${index}`).textContent = skillPoints;
+      // Check either a new skill value fits in the correct range.
+      if (skillPointsRemaining) {
+        if (skillPoints >= 1 && skillPoints <= 5 && skillPointsRemaining - skillPoints >= 0) {
           skillPointsRemaining -= skillPoints;
+          document.querySelector(`.skill-level-${index}`).textContent = skillPoints;
           document.querySelector('.skill-points-remaining').textContent = skillPointsRemaining;
-          if (debug) console.log(`skillPointsRemaining: ${skillPointsRemaining}`);
-        } else {
-          // FIXME add a <p> to warn user to enter a score between 1 and 5 for each wrong skill value.
-          const newP = document.createElement('p');
-          const invalidSkillLevel = document.createTextNode('The skill score can not exceed 5!.');
-          newP.appendChild(invalidSkillLevel);
-          document.body.insertBefore(newP, skillInfoElm);
-          console.info(invalidSkillLevel);
+
+          // Warn the user and deactivate train button when no SP left.
+          if (!skillPointsRemaining) {
+            document.querySelector('.skill-points-info').textContent =
+              'ℹ️ No skill points available.';
+            allSkillPointsSpent = true;
+          }
         }
-      } else if (skillPointsRemaining <= 0) {
-        document.querySelector('.skill-points-info').textContent = 'ℹ️ No skill points available.';
-        allSkillPointsSpent = true;
       }
+      //   } else {
+      //     // FIXME add a <p> to warn user to enter a level between 1 and 5 for each wrong skill value.
+      //     This eval path is never reached!
+      //     TODO add it in its own handler function
+      //     const newP = document.createElement('p');
+      //     const invalidSkillLevel = document.createTextNode('The skill level can not exceed 5!.');
+      //     newP.appendChild(invalidSkillLevel);
+      //     document.body.insertBefore(newP, skillInfoElm);
+      //     console.info(invalidSkillLevel);
     }
     // Reset user input to prevent awkwardnesses.
     skillElm.value = '';
