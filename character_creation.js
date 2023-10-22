9d92bb678d3e463d7c2e163bba503901e84d22f8
aurèle <aurelien.plazzotta@tutanota.com>
2023-10-22 10:48:54 +0200

    Fixing partially wrong assignments of skillPoints to skill-score tags

--- character_creation.js
+++ character_creation.js
@@ -1,14 +1,14 @@
 'use strict';
 
 // HTML tags' selections.
-const btnTrain = document.querySelector('.train');
+const btnTrain = document.getElementById('train');
 const skillsElm = document.querySelectorAll('.skill');
 const skills = ['strength', 'dexterity', 'stamina', 'luck'];
-// const skillPointsRemaining = document.querySelector('.skill-points-remaining')
 
 // State variables.
 let skillPointsRemaining = 20;
 let allSkillPointsSpent = false;
+let debug = true;
 
 const addSkillPoints = function (/** @type {string} */ skill) {
   /**
@@ -20,27 +20,32 @@ const addSkillPoints = function (/** @type {string} */ skill) {
 /* TODO: prevent skill points remaining to fall under 0 
 by checking all `skillElm.value` when clicking `train` button.
 */
-// for (let i = 0; i < skillsElm.length; ++i) {
-skillsElm.forEach((skillElm, index) => {
-  btnTrain.addEventListener('click', function () {
-    skillPointsRemaining -= Number(skillElm.value);
-
+btnTrain.addEventListener('click', function () {
+  skillsElm.forEach((skillElm, index) => {
+    // FIXME calcul is wrong
+    // skillPointsRemaining -= Number(skillElm.value);
+    // if (debug) console.log(`skillPointsRemaining: ${skillPointsRemaining}`);
     if (!allSkillPointsSpent) {
       const skillPoints = addSkillPoints(`${skills[index]}`);
-
-      if (skillPoints > 0 && skillPoints < 6) {
-        document.querySelector(`.skill-score-${index}`).textContent = skillPoints;
-
-        if (skillPointsRemaining > 0) {
+      if (debug) console.info(`skillPoints: ${skillPoints}`);
+      if (skillPointsRemaining > 0) {
+        if (skillPoints >= 1 && skillPoints <= 5) {
+          document.querySelector(`.skill-score-${index}`).textContent = skillPoints;
           skillPointsRemaining -= skillPoints;
           document.querySelector('.skill-points-remaining').textContent = skillPointsRemaining;
+          if (debug) console.log(`skillPointsRemaining: ${skillPointsRemaining}`);
         } else {
-          document.querySelector('.points-left').textContent = 'ℹ️ No skill points available.';
-          allSkillPointsSpent = true;
+          // FIXME add a <p> to warn user to enter a score between 1 and 5 for each wrong skill value.
+          const skillPointsInvalid = document.createElement('p');
+          skillPointsInvalid.textContent = 'Enter a score between 1 and 5.';
+          console.log(skillPointsInvalid);
         }
-
-        // TODO add a <p> to warn user to enter a score between 1 and 5 for each wrong skill value.
+      } else if (skillPointsRemaining <= 0) {
+        document.querySelector('.skill-points-info').textContent = 'ℹ️ No skill points available.';
+        allSkillPointsSpent = true;
       }
     }
+    // Reset user input to prevent awkwardnesses.
+    skillElm.value = '';
   });
 });
